2d9aafe8ac9ce5df0e487f5d49a5e61c77de0880
Shiv Raj Badu <shivrajbadu@gmail.com>
2018-05-03 20:11:14 +0545

    setup and configuring reactjs application in basic advance level which having webpack.config.js, package.json, index.html, main.js, app.jsx, etc

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "installation started"
+sudo npm install -g babel
+sudo npm install -g babel-cli
+sudo npm install webpack --save
+sudo npm install webpack-dev-server --save
+sudo npm install react --save
+sudo npm install react-dom --save
+sudo npm install babel-core
+sudo npm install babel-loader
+sudo npm install babel-preset-react
+sudo npm install babel-preset-es2015
+echo "installation finished"
